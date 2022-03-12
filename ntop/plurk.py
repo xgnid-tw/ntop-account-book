6ad8688cfd6ad79fbcc411b1fa0dd0940cec5ca4
xgnid <e19940604@gmail.com>
2022-03-12 23:36:21 +0900

    circleci: remove pip install

--- ntop/plurk.py
+++ ntop/plurk.py
@@ -1,10 +1,15 @@
 from plurk_oauth import PlurkAPI
+import os
 
 class PlurkHelper:
   plurk = None
   def __init__(self):
-    self.plurk = PlurkAPI("PLURK_USER_KEY", "PLURK_USER_SECRET")
-    self.plurk.authorize("PLURK_API_TOKEN","PLURK_API_SECRET")
+    PLURK_USER_KEY    = os.environ['PLURK_USER_KEY']
+    PLURK_USER_SECRET = os.environ['PLURK_USER_SECRET']
+    PLURK_API_TOKEN   = os.environ['PLURK_API_TOKEN']
+    PLURK_API_SECRET  = os.environ['PLURK_API_SECRET']
+    self.plurk = PlurkAPI(PLURK_USER_KEY, PLURK_USER_SECRET)
+    self.plurk.authorize(PLURK_API_TOKEN,PLURK_API_SECRET)
 
   def call_private_plurk(self,target):
     self.plurk.call()
