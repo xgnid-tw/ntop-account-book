4a84b06e40a7c0df9cc56f8468d918564991438a
xgnid <e19940604@gmail.com>
2022-05-31 00:45:50 +0900

    revert

--- ntop/main.py
+++ ntop/main.py
@@ -19,9 +19,9 @@ for r in result['results']:
 
 rabbit_publisher = RabbitHelper()
 # only execute at 1 and 16 every month
-"""today =  datetime.date.today()
+today =  datetime.date.today()
 if today.day not in (1,16):
-    sys.exit()"""
+    sys.exit()
 
 # check each notion database whethere there is someone's debit over 2000 and two month
 for discord_id, notion_id in discord_to_notion.items():
@@ -35,7 +35,7 @@ for discord_id, notion_id in discord_to_notion.items():
         notion_url = f'https://www.notion.so/{notion_id}'
         # send meesage to rabbit mq , the it will send discord DM to that guy
         json_message = json.JSONEncoder().encode({
-            'user_id': 374867612519366657, #temperary change to me
+            'user_id': discord_id, #temperary change to me
             'message': f'[欠費提醒] {notion_url} (回訊息機器人看不到，如果有漏登聯絡一下XG) ',
         })
         rabbit_publisher.send(json_message)
